dc60ca80de5f600acafdf254aba09cdb67f06f1d
Damien Daspit <damien_daspit@sil.org>
2020-08-14 13:59:41 +0700

    Update to version 1.1.1

--- Source/Graph#.Controls/Properties/AssemblyInfo.cs
+++ Source/Graph#.Controls/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GraphSharp.Controls")]
-[assembly: AssemblyCopyright("Copyright @  2007")]
+[assembly: AssemblyCopyright("Copyright @ 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
@@ -59,5 +59,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
